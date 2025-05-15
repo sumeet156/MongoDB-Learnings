5c35a59249141f0f6f5f5f2d4b7560282b7412e5
Sumeet Choudhary <131875585+sumeet156@users.noreply.github.com>
2025-05-15 15:17:34 +0530

    Create Unit 08: MongoDB CRUD Operations in Node.js

---
+++ Unit 08: MongoDB CRUD Operations in Node.js
@@ -0,0 +1,27 @@
+   MongoDB CRUD Operations in Node.js
+     
+            Lesson 01: Working with MongoDB Documents in Node.js
+                    -  BSON: Optimized for storage, retrieval, and transmission across the wire. More secure than plain text JSON
+                             Supports More data types than JSON.
+                             BSON-encoded documents are converted automatically by the driver. This means that you can use the data immediately in your application as 
+                                  normal JSON and access properties by using dot notation. The driver handles the conversion from BSON to JSON for you.
+                     
+                      ~ Documents are the foundational organizing principle of storing data in MongoDB because they provide flexible and concise data representation.
+                        Use Correct data types for field values : docs- 
+
+
+            Lesson 02: Inserting a Document in Node.js Applications
+                     - 
+
+            Lesson 03: Querying a MongoDB Collection in Node.js Applications
+
+
+            Lesson 04: Updating Documents in Node.js Applications
+
+
+            Lesson 05: Deleting Documents in Node.js Applications
+
+
+            Lesson 06: Creating MongoDB Transactions in Node.js Applications
+
+
